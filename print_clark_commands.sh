5296efbcb6703697051a49b44f318488121e22a9
Andrei Gaponenko <gandr@fnal.gov>
2015-12-23 14:57:52 -0800

    Include the "cd" into the generated commands.

--- print_clark_commands.sh
+++ print_clark_commands.sh
@@ -2,4 +2,6 @@
 #
 # bash one-liners have to be put in scripts because MOFIA env is only available in csh.
 #
-while read fn ; do echo ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
+
+# 20151221: while read fn ; do echo ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
+while read fn ; do echo cd `pwd` \&\& ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
--- print_mofia_commands.sh
+++ print_mofia_commands.sh
@@ -2,4 +2,4 @@
 #
 # bash one-liners have to be put in scripts because MOFIA env is only available in csh.
 #
-while read fn ; do echo ~/bin/parallel/mofia.sh ${1:?kcm-file arg missing} $fn ; done
+while read fn ; do echo "cd `pwd` && ~/bin/parallel/mofia.sh ${1:?kcm-file arg missing} $fn" ; done
