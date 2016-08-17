896c4e850e3adc1fdf6fc3a324ef753f655a19dc
Andrei Gaponenko <gandr@fnal.gov>
2016-08-16 21:53:11 -0700

    Allow several input files per Clark job.

--- clark.sh
+++ clark.sh
@@ -9,38 +9,38 @@ absname() {
     esac
 }
 
-
 exe=${1:?Clark binary arg missing}
 exe=$(absname $exe)
 if [ ! -x $exe ]; then
     echo "Error: file $exe is not executable" >&2
     exit 1
 fi
+shift
 
-
-
-cfg=${2:?CFG file arg missing}
+cfg=${1:?CFG file arg missing}
 cfg=$(absname $cfg)
 if [ ! -r $cfg ]; then
     echo "Can't read command file $cfg" >&2
     exit 1
 fi
+shift
 
-tree=${3:?Data file arg missing}
+tree=${1:?Data file arg missing}
 tree=$(absname $tree)
 if [ ! -r $tree ]; then
     echo "Can't read data file $tree" >&2
     exit 1
 fi
+### no shift here - data files are now $@
 
 bn=$(basename $tree .root)
 bn=${bn##tree}
 
 echo "CFG = $cfg"
-echo "Data file = $tree"
+echo "Data files = $@"
 echo "bn = $bn"
 if mkdir cr$bn && cd cr$bn; then
-    /usr/bin/time ${exe} -l clark$bn.log -o clark$bn.root $cfg $tree > job.log 2>&1
+    /usr/bin/time ${exe} -l clark$bn.log -o clark$bn.root $cfg "$@" > job.log 2>&1
     ret=$?
     echo "Clark exit status: $ret" >> job.log
     exit $ret
--- print_clark_commands.sh
+++ print_clark_commands.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 #
-# bash one-liners have to be put in scripts because MOFIA env is only available in csh.
 #
-
 # 20151221: while read fn ; do echo ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
-while read fn ; do echo cd `pwd` \&\& ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
+# 20160816: while read fn ; do echo cd `pwd` \&\& ~/bin/parallel/clark.sh ${1:?exe-file arg missing}  ${2:?config-file arg missing} $fn ; done
+
+print_clark_cmd() {
+    echo cd `pwd` \&\& ~/bin/parallel/clark.sh "$@"
+}
+
+exe=${1:?exe-file arg missing}
+conf=${2:?config-file arg missing}
+chunksize=${3:?chunksize arg is missing}
+
+inputs=()
+
+while read fn ; do inputs=("${inputs[@]}" "$fn"); if [ ${#inputs[@]} -ge "$chunksize" ]; then print_clark_cmd "$exe" "$conf" "${inputs[@]}"; inputs=(); fi; done
+
+if [ ${#inputs[@]} -gt 0 ]; then 
+    print_clark_cmd "$exe" "$conf" "${inputs[@]}"; inputs=();
+fi
